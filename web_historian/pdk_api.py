e8ed71beae20974d00538c5f28a618c041755298
Chris J. Karr <github@audacious-software.com>
2019-03-25 12:37:50 +0000

    Added date range support to custom exporters.
    * Updated PDK dependency.

--- web_historian/pdk_api.py
+++ web_historian/pdk_api.py
@@ -83,7 +83,7 @@ def viz_template(source, identifier):
 
     return None
 
-def compile_report(generator, sources): # pylint: disable=too-many-branches, too-many-statements
+def compile_report(generator, sources, data_start=None, data_end=None): # pylint: disable=too-many-branches, too-many-statements, too-many-locals
     if generator == 'web-historian':
         filename = tempfile.gettempdir() + '/pdk_' + generator + '.txt'
 
@@ -93,7 +93,15 @@ def compile_report(generator, sources): # pylint: disable=too-many-branches, too
             writer.writerow(['Source', 'Generator', 'Generator Identifier', 'Created Timestamp', 'Created Date', 'Recorded Timestamp', 'Recorded Date', 'Visit ID', 'URL ID', 'Referrer ID', 'Domain', 'Protocol', 'URL', 'Title', 'Top Domain', 'Search Terms', 'Transition Type', 'Timestamp', 'Date'])
 
             for source in sources:
-                points = DataPoint.objects.filter(source=source, generator_identifier=generator).order_by('created')
+                points = DataPoint.objects.filter(source=source, generator_identifier=generator)
+
+                if data_start is not None:
+                    points = points.filter(created__gte=data_start)
+
+                if data_end is not None:
+                    points = points.filter(created__lte=data_end)
+
+                points = points.order_by('source', 'created')
 
                 index = 0
                 count = points.count()
@@ -196,7 +204,15 @@ def compile_report(generator, sources): # pylint: disable=too-many-branches, too
             ])
 
             for source in sources:
-                points = DataPoint.objects.filter(source=source, generator_identifier=generator).order_by('created')
+                points = DataPoint.objects.filter(source=source, generator_identifier=generator)
+
+                if data_start is not None:
+                    points = points.filter(created__gte=data_start)
+
+                if data_end is not None:
+                    points = points.filter(created__lte=data_end)
+
+                points = points.order_by('source', 'created')
 
                 index = 0
                 count = points.count()
